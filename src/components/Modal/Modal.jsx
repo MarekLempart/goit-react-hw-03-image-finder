554671be6ba0e29275b3f36754ce9f3157842d70
MarekLempart <kama.zakupy@gmail.com>
2024-04-26 20:11:27 +0200

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,55 +1,5 @@
 // Modal.jsx
 
-// import * as basicLightbox from 'basiclightbox';
-// import { useEffect, useRef } from 'react';
-// import css from './Modal.module.css';
-
-// const Modal = ({ showModal, closeModal, largeImageURL, alt }) => {
-//   const modalRef = useRef(null);
-
-//   useEffect(() => {
-//     const handleOutsideClick = evt => {
-//       if (modalRef.current && !modalRef.current.contains(evt.target)) {
-//         closeModal();
-//       }
-//     };
-
-//     const handleEscPress = evt => {
-//       if (evt.keyCode === 27) {
-//         closeModal();
-//       }
-//     };
-
-//     if (showModal) {
-//       document.addEventListener('mousedown', handleOutsideClick);
-//       document.addEventListener('keydown', handleEscPress);
-
-//       const instance = basicLightbox.create(`
-//         <img src="${largeImageURL}" alt="${alt}" />
-//       `);
-//       instance.show();
-
-//       return () => {
-//         document.removeEventListener('mousedown', handleOutsideClick);
-//         document.removeEventListener('keydown', handleEscPress);
-//         instance.close();
-//       };
-//     }
-//   }, [showModal, largeImageURL, alt, closeModal]);
-
-//   if (!showModal) return null;
-
-//   return (
-//     <div className={css.Overlay}>
-//       <div className={css.Modal} ref={modalRef}>
-//         <img src={largeImageURL} alt={alt} />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Modal;
-
 import { useEffect } from 'react';
 import css from './Modal.module.css';
 
